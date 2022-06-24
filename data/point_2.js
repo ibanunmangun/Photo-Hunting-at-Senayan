8a85b55b9b37c9606a9639f266eebc4316c8babf
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 22:36:24 +0800

    Add files via upload

---
+++ data/point_2.js
@@ -0,0 +1,11 @@
+var json_point_2 = {
+"type": "FeatureCollection",
+"name": "point_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Hutan Kota GBK", "desc": "Hutan Kota GBK, 1, RT.1\/RW.3, Gelora, Tanah Abang, Central Jakarta City, Jakarta 10270, Indonesia", "Y": "-6.2220939", "X": "106.807348" }, "geometry": { "type": "Point", "coordinates": [ 106.8073483, -6.2220939 ] } },
+{ "type": "Feature", "properties": { "name": "Gelora Bung Karno", "desc": "Gelora Bung Karno Main Stadium, Jl. Pintu Satu Senayan, Gelora, Tanah Abang, Central Jakarta City, Jakarta 10270, Indonesia", "Y": "-6.2185701", "X": "106.801796" }, "geometry": { "type": "Point", "coordinates": [ 106.8017969, -6.2185701 ] } },
+{ "type": "Feature", "properties": { "name": "Cork & Screw Country Club", "desc": "CORK&SCREW COUNTRY CLUB, Avenue, Senayan, Jl. Asia Afrika, Pintu IX, Jakarta, Daerah Khusus Ibukota Jakarta 10270, Indonesia", "Y": "-6.2259329", "X": "106.797939" }, "geometry": { "type": "Point", "coordinates": [ 106.7979397, -6.2259329 ] } },
+{ "type": "Feature", "properties": { "name": "JPO (Jembatan Penyeberangan Orang) Gelora Bung Karno", "desc": "JPO (Jembatan Penyeberangan Orang) Gelora Bung Karno, Jl. Jenderal Sudirman No.52-53, RT.5\/RW.3, Senayan, Kebayoran Baru, South Jakarta City, Jakarta 12190, Indonesia", "Y": "-6.2243396", "X": "106.806066" }, "geometry": { "type": "Point", "coordinates": [ 106.8060662, -6.2243396 ] } }
+]
+}
